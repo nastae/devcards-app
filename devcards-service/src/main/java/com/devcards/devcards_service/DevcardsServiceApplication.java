daf05e44348a156079987bbdb2612654601338c5
Aurimas <saauris@gmail.com>
2026-05-15 20:48:12 +0300

    Initial commit

---
+++ devcards-service/src/main/java/com/devcards/devcards_service/DevcardsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devcards.devcards_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcardsServiceApplication.class, args);
+	}
+
+}
---
+++ devcards-service/src/test/java/com/devcards/devcards_service/DevcardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcards.devcards_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
